ede80195ab159bdc33e35626cd9ba22e591ed0a9
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-03-17 15:29:05 +0300

    Exercise_13_complete

---
+++ 2th_seminar/Exercise_13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+string someNumbers = Console.ReadLine()!;
+if (someNumbers.Length < 3)
+{
+    Console.WriteLine("Не трехзначное число");
+    return;
+}
+Console.WriteLine(someNumbers[2]);
