b95ad095deadc923fa3ba52c908b8ca974e55156
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-03-24 20:38:10 +0300

    Exercise_27a

--- 4th_seminar/Exercise_27/Program.cs
+++ 4th_seminar/Exercise_27/Program.cs
@@ -6,31 +6,47 @@
 
 // 9012 -> 12
 
-void Main(int args)
-{
+// void Main(int args)
+// {
         
-        int res = 0;
-        Sum = 0;
-        while (args > 0)
-        {
-            res+= args % 10;
-            args /= 10;
-            if(args > 0)
-            {
-           res += Sum(args);
-           return result;
-            }
-        }
+//         int res = 0;
+//         Sum = 0;
+//         while (args > 0)
+//         {
+//             res+= args % 10;
+//             args /= 10;
+//             if(args > 0)
+//             {
+//            res += Sum(args);
+//            return result;
+//             }
+//         }
 
-}       
-Console.Write("Введите число N: ");
-string number = Console.ReadLine();
-int numb = int.Parse(number);
-sum = Main(numb);
-Console.WriteLine(sum);
+// }       
+// Console.Write("Введите число N: ");
+// string number = Console.ReadLine();
+// int numb = int.Parse(number);
+// sum = Main(numb);
+// Console.WriteLine(sum);
 
 
 
 // string? input = Console.ReadLine()!;
 // int []res = int.Parse(input);
 //Console.WriteLine(Main(input));
+
+int SumNum(string number)
+{
+    int sum = 0;
+    for(int i=0;i<=number.Length;i++)
+    {
+        sum += number[i];
+        return sum;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число");
+string number = (Console.ReadLine()!);
+SumNum(number);
+Console.WriteLine("SumNum: " + SumNum(number));
+
