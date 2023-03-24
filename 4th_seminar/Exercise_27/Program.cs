c20cd822cf1664d7b0d36700d2b82565ff23a8ab
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-03-24 19:58:35 +0300

    Exercise_27

---
+++ 4th_seminar/Exercise_27/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+void Main(int args)
+{
+        
+        int res = 0;
+        Sum = 0;
+        while (args > 0)
+        {
+            res+= args % 10;
+            args /= 10;
+            if(args > 0)
+            {
+           res += Sum(args);
+           return result;
+            }
+        }
+
+}       
+Console.Write("Введите число N: ");
+string number = Console.ReadLine();
+int numb = int.Parse(number);
+sum = Main(numb);
+Console.WriteLine(sum);
+
+
+
+// string? input = Console.ReadLine()!;
+// int []res = int.Parse(input);
+//Console.WriteLine(Main(input));
