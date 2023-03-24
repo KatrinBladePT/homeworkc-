8c7c66fbf11d31cc1e189996786ad306a563c297
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-03-24 21:15:31 +0300

    Exercise_27_complete

--- 4th_seminar/Exercise_27/Program.cs
+++ 4th_seminar/Exercise_27/Program.cs
@@ -6,6 +6,32 @@
 
 // 9012 -> 12
 
+
+int SumNum(string number)
+{
+    int sum = 0;
+    for(int i=0;i<number.Length;i++)
+    {
+        sum += Int32.Parse(number[i].ToString()); // добавлена конвертация символа в число
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+Console.WriteLine("SumNum: " + SumNum(number));
+
+
+
+
+
+
+
+
+
+
+
+
 // void Main(int args)
 // {
         
@@ -35,18 +61,17 @@
 // int []res = int.Parse(input);
 //Console.WriteLine(Main(input));
 
-int SumNum(string number)
-{
-    int sum = 0;
-    for(int i=0;i<=number.Length;i++)
-    {
-        sum += number[i];
-        return sum;
-    }
-    return sum;
-}
-Console.WriteLine("Введите число");
-string number = (Console.ReadLine()!);
-SumNum(number);
-Console.WriteLine("SumNum: " + SumNum(number));
+// int SumNum([] number)
+// {
+//     int sum = 0;
+//     for(int i=0;i<number.Length;i++)
+//     {
+//         sum += number[i];
+//     }
+//     return sum;
+// }
+// Console.WriteLine("Введите число");
+// int []number = new []{int.Parse(Console.ReadLine()!)};
+// SumNum(number);
+// Console.WriteLine("SumNum: " + SumNum(number));
 
