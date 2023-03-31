38a30b8682a0c59e8b810854461ced20c7899aca
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-03-31 13:47:33 +0300

    Exercise_41_complete

--- 6th_seminar/Exercise_41/Program.cs
+++ 6th_seminar/Exercise_41/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+class Program
+{
+    static void Main(string[] args) {
+        Console.Write("Введите количество значений для ввода: ");
+        int.TryParse(Console.ReadLine(), out int m);
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            Console.Write("Введите значение {0}: ");
+            int.TryParse(Console.ReadLine(),out int number);
+
+            if (number > 0) {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Количество значений больше 0: {count}");
+    }
+}
