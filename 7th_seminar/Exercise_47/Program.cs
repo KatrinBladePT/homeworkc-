ce3334bb37cad1e8d68ac38ffc6ad4affeb134d9
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-04-04 09:35:39 +0300

    Exercise_47_complete

---
+++ 7th_seminar/Exercise_47/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами
+
+
+
+int m = 3;
+int n = 4;
+double[,] table = new double[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        table[i, j] = new Random().NextDouble();
+    }
+}
