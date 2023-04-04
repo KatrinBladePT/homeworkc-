c61d2d1b72367f1b4beb0f5cb86b9cf302f5e760
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-04-04 11:29:50 +0300

    Exercise_52_complete

---
+++ 7th_seminar/Exercise_52/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее 
+// арифметическое элементов в каждом столбце.
+
+
+int[,] array = new int[,] 
+{ 
+    { 1, 2, 3 }, 
+    { 4, 5, 6 }, 
+    { 7, 8, 9 } 
+};
+
+double[] columnAverage = new double[array.GetLength(1)];
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    columnAverage[j] = (double)sum / array.GetLength(0);
+}
+
+// Вывод результатов
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    Console.WriteLine("Среднее арифметическое столбца {0}: {1}", j + 1, columnAverage[j]);
+}
