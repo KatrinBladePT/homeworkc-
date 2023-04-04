20277bb7a3322528c94ae54a926ffd5cc35f15d0
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-04-04 10:02:06 +0300

    Exercise_50_complete

---
+++ 7th_seminar/Exercise_50/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном 
+// массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+
+    int[,] array = new int [,] {
+        {1, 4, 7, 2},
+        {5, 9, 2, 3},
+        {8, 4, 2, 4}
+    };
+        
+    Console.WriteLine("Введите позиции элемента в двумерном массиве:");
+    Console.Write("Строка: ");
+    int.TryParse(Console.ReadLine()!, out int row);
+    Console.Write("Столбец: ");
+    int.TryParse(Console.ReadLine()!, out int column);    
+    if (row < 0 || row >= array.GetLength(0) || column < 0 || column >= array.GetLength(1)) {
+            Console.WriteLine("Такого элемента нет.");
+    }
+    else {
+            Console.WriteLine($"Элемент [{row}, {column}] = {array[row, column]}");
+    }
