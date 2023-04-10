5331f2c23e02cb8aa8bf904588641f321ce4671e
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-04-10 04:45:27 +0300

    Exercise_66_complete

---
+++ 9th_seminar/Exercise_66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+
+using System;
+
+namespace SumOfNaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n, sum = 0;
+
+            Console.Write("Введите значение M: ");
+            m = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение N: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = m; i <= n; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"Сумма натуральных чисел от {m} до {n}: {sum}");
+        }
+    }
+} 
